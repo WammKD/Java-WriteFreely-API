e8a5cbc0939b2cfd30c012f457af20816e5606a8
Wamm K. D <jaft.r@outlook.com>
2018-12-07 22:26:52 -0600

    Some extra endpoints making use of the objects I added; I need to check if the parameters are supposed to be query strings or part of the object sent. I'll have to circle back after asking.

--- src/main/java/fediverse/writefreely/api/Endpoints.java
+++ src/main/java/fediverse/writefreely/api/Endpoints.java
@@ -2,22 +2,28 @@ package fediverse.writefreely.api;
 
 import fediverse.writefreely.api.model.Auth;
 import fediverse.writefreely.api.model.Collection;
+import fediverse.writefreely.api.model.Post;
 import fediverse.writefreely.api.model.ResponseWrapper;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 interface Endpoints {
-	static final String AUTH       = "api/auth/login";
-	static final String POST       = "api/posts/{postID}";
-	static final String COLLECTION = "api/collections/{collectionAlias}";
+	static final String AUTH          = "api/auth/login";
+	static final String POST_PUBLISH  = "api/posts";
+	static final String POST_RETRIEVE = "api/posts/{postID}";
+	static final String COLLECTION    = "api/collections/{collectionAlias}";
 
 	@POST(Endpoints.AUTH)
 	Call<ResponseWrapper<Auth>>       getToken();
 
-	@POST(Endpoints.POST)
-	Call<ResponseWrapper<Auth>>       createPost();
+	@POST(Endpoints.POST_PUBLISH)
+	Call<ResponseWrapper<Post>>       publishPost(@Body Post body);
+
+	@GET(Endpoints.POST_RETRIEVE)
+	Call<ResponseWrapper<Post>>       retrievePost(@Path("postID") String pID);
 
 	@GET(Endpoints.COLLECTION)
 	Call<ResponseWrapper<Collection>> getCollection(@Path("collectionAlias") String ca);
