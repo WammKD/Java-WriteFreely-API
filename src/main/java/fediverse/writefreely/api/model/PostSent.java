12b6cdea8685bf05d4e6b5d309e9bedabe552857
Wamm K. D <jaft.r@outlook.com>
2018-12-09 06:47:20 -0600

    Update all of the `Pair`s to arrays of my new class.

--- src/main/java/fediverse/writefreely/api/model/PostSent.java
+++ src/main/java/fediverse/writefreely/api/model/PostSent.java
@@ -2,24 +2,23 @@ package fediverse.writefreely.api.model;
 
 import java.time.ZonedDateTime;
 import java.util.Locale;
-import javafx.util.Pair;
 
 public class PostSent {
-	private final String        body;
-	private final String        title;
-	private final Appearance    font;
-	private final Locale        lang;
-	private final Boolean       rtl;
-	private final ZonedDateTime created;
-	private final Pair<CrosspostID, String> crosspost;
+	private final String          body;
+	private final String          title;
+	private final Appearance      font;
+	private final Locale          lang;
+	private final Boolean         rtl;
+	private final ZonedDateTime   created;
+	private final CrosspostInfo[] crosspost;
 
-	public PostSent(final String        body,
-	                final String        title,
-	                final Appearance    font,
-	                final Locale        lang,
-	                final Boolean       rtl,
-	                final ZonedDateTime created,
-	                final Pair<CrosspostID, String>      crosspost) {
+	public PostSent(final String          body,
+	                final String          title,
+	                final Appearance      font,
+	                final Locale          lang,
+	                final Boolean         rtl,
+	                final ZonedDateTime   created,
+	                final CrosspostInfo[] crosspost) {
 		this.body      = body;
 		this.title     = title;
 		this.font      = font;
@@ -38,48 +37,48 @@ public class PostSent {
 		this(body, title, font, lang, rtl, created, null);
 	}
 
-	public PostSent(final String     body,
-	                final String     title,
-	                final Appearance font,
-	                final Locale     lang,
-	                final Boolean    rtl,
-	                final Pair<CrosspostID, String>   crosspost) {
+	public PostSent(final String          body,
+	                final String          title,
+	                final Appearance      font,
+	                final Locale          lang,
+	                final Boolean         rtl,
+	                final CrosspostInfo[] crosspost) {
 		this(body, title, font, lang, rtl, null, crosspost);
 	}
 
-	public PostSent(final String        body,
-	                final String        title,
-	                final Appearance    font,
-	                final Locale        lang,
-	                final ZonedDateTime created,
-	                final Pair<CrosspostID, String>      crosspost) {
+	public PostSent(final String          body,
+	                final String          title,
+	                final Appearance      font,
+	                final Locale          lang,
+	                final ZonedDateTime   created,
+	                final CrosspostInfo[] crosspost) {
 		this(body, title, font, lang, null, created, crosspost);
 	}
 
-	public PostSent(final String        body,
-	                final String        title,
-	                final Appearance    font,
-	                final Boolean       rtl,
-	                final ZonedDateTime created,
-	                final Pair<CrosspostID, String>      crosspost) {
+	public PostSent(final String          body,
+	                final String          title,
+	                final Appearance      font,
+	                final Boolean         rtl,
+	                final ZonedDateTime   created,
+	                final CrosspostInfo[] crosspost) {
 		this(body, title, font, null, rtl, created, crosspost);
 	}
 
-	public PostSent(final String        body,
-	                final String        title,
-	                final Locale        lang,
-	                final Boolean       rtl,
-	                final ZonedDateTime created,
-	                final Pair<CrosspostID, String>      crosspost) {
+	public PostSent(final String          body,
+	                final String          title,
+	                final Locale          lang,
+	                final Boolean         rtl,
+	                final ZonedDateTime   created,
+	                final CrosspostInfo[] crosspost) {
 		this(body, title, null, lang, rtl, created, crosspost);
 	}
 
-	public PostSent(final String        body,
-	                final Appearance    font,
-	                final Locale        lang,
-	                final Boolean       rtl,
-	                final ZonedDateTime created,
-	                final Pair<CrosspostID, String>      crosspost) {
+	public PostSent(final String          body,
+	                final Appearance      font,
+	                final Locale          lang,
+	                final Boolean         rtl,
+	                final ZonedDateTime   created,
+	                final CrosspostInfo[] crosspost) {
 		this(body, null, font, lang, rtl, created, crosspost);
 	}
 
@@ -99,11 +98,11 @@ public class PostSent {
 		this(body, title, font, lang, null, created, null);
 	}
 
-	public PostSent(final String     body,
-	                final String     title,
-	                final Appearance font,
-	                final Locale     lang,
-	                final Pair<CrosspostID, String>   crosspost) {
+	public PostSent(final String          body,
+	                final String          title,
+	                final Appearance      font,
+	                final Locale          lang,
+	                final CrosspostInfo[] crosspost) {
 		this(body, title, font, lang, null, null, crosspost);
 	}
 
@@ -115,19 +114,19 @@ public class PostSent {
 		this(body, title, font, null, rtl, created, null);
 	}
 
-	public PostSent(final String     body,
-	                final String     title,
-	                final Appearance font,
-	                final Boolean    rtl,
-	                final Pair<CrosspostID, String>   crosspost) {
+	public PostSent(final String          body,
+	                final String          title,
+	                final Appearance      font,
+	                final Boolean         rtl,
+	                final CrosspostInfo[] crosspost) {
 		this(body, title, font, null, rtl, null, crosspost);
 	}
 
-	public PostSent(final String        body,
-	                final String        title,
-	                final Appearance    font,
-	                final ZonedDateTime created,
-	                final Pair<CrosspostID, String>      crosspost) {
+	public PostSent(final String          body,
+	                final String          title,
+	                final Appearance      font,
+	                final ZonedDateTime   created,
+	                final CrosspostInfo[] crosspost) {
 		this(body, title, font, null, null, created, crosspost);
 	}
 
@@ -139,27 +138,27 @@ public class PostSent {
 		this(body, title, null, lang, rtl, created, null);
 	}
 
-	public PostSent(final String   body,
-	                final String   title,
-	                final Locale   lang,
-	                final Boolean  rtl,
-	                final Pair<CrosspostID, String> crosspost) {
+	public PostSent(final String          body,
+	                final String          title,
+	                final Locale          lang,
+	                final Boolean         rtl,
+	                final CrosspostInfo[] crosspost) {
 		this(body, title, null, lang, rtl, null, crosspost);
 	}
 
-	public PostSent(final String        body,
-	                final String        title,
-	                final Locale        lang,
-	                final ZonedDateTime created,
-	                final Pair<CrosspostID, String>      crosspost) {
+	public PostSent(final String          body,
+	                final String          title,
+	                final Locale          lang,
+	                final ZonedDateTime   created,
+	                final CrosspostInfo[] crosspost) {
 		this(body, title, null, lang, null, created, crosspost);
 	}
 
-	public PostSent(final String        body,
-	                final String        title,
-	                final Boolean       rtl,
-	                final ZonedDateTime created,
-	                final Pair<CrosspostID, String>      crosspost) {
+	public PostSent(final String          body,
+	                final String          title,
+	                final Boolean         rtl,
+	                final ZonedDateTime   created,
+	                final CrosspostInfo[] crosspost) {
 		this(body, title, null, null, rtl, created, crosspost);
 	}
 
@@ -171,35 +170,35 @@ public class PostSent {
 		this(body, null, font, lang, rtl, created, null);
 	}
 
-	public PostSent(final String     body,
-	                final Appearance font,
-	                final Locale     lang,
-	                final Boolean    rtl,
-	                final Pair<CrosspostID, String>   crosspost) {
+	public PostSent(final String          body,
+	                final Appearance      font,
+	                final Locale          lang,
+	                final Boolean         rtl,
+	                final CrosspostInfo[] crosspost) {
 		this(body, null, font, lang, rtl, null, crosspost);
 	}
 
-	public PostSent(final String        body,
-	                final Appearance    font,
-	                final Locale        lang,
-	                final ZonedDateTime created,
-	                final Pair<CrosspostID, String>      crosspost) {
+	public PostSent(final String          body,
+	                final Appearance      font,
+	                final Locale          lang,
+	                final ZonedDateTime   created,
+	                final CrosspostInfo[] crosspost) {
 		this(body, null, font, lang, null, created, crosspost);
 	}
 
-	public PostSent(final String        body,
-	                final Appearance    font,
-	                final Boolean       rtl,
-	                final ZonedDateTime created,
-	                final Pair<CrosspostID, String>      crosspost) {
+	public PostSent(final String          body,
+	                final Appearance      font,
+	                final Boolean         rtl,
+	                final ZonedDateTime   created,
+	                final CrosspostInfo[] crosspost) {
 		this(body, null, font, null, rtl, created, crosspost);
 	}
 
-	public PostSent(final String        body,
-	                final Locale        lang,
-	                final Boolean       rtl,
-	                final ZonedDateTime created,
-	                final Pair<CrosspostID, String>      crosspost) {
+	public PostSent(final String          body,
+	                final Locale          lang,
+	                final Boolean         rtl,
+	                final ZonedDateTime   created,
+	                final CrosspostInfo[] crosspost) {
 		this(body, null, null, lang, rtl, created, crosspost);
 	}
 
@@ -224,10 +223,10 @@ public class PostSent {
 		this(body, title, font, null, null, created, null);
 	}
 
-	public PostSent(final String     body,
-	                final String     title,
-	                final Appearance font,
-	                final Pair<CrosspostID, String>   crosspost) {
+	public PostSent(final String          body,
+	                final String          title,
+	                final Appearance      font,
+	                final CrosspostInfo[] crosspost) {
 		this(body, title, font, null, null, null, crosspost);
 	}
 
@@ -245,10 +244,10 @@ public class PostSent {
 		this(body, title, null, lang, null, created, null);
 	}
 
-	public PostSent(final String body,
-	                final String title,
-	                final Locale lang,
-	                final Pair<CrosspostID, String> crosspost) {
+	public PostSent(final String          body,
+	                final String          title,
+	                final Locale          lang,
+	                final CrosspostInfo[] crosspost) {
 		this(body, title, null, lang, null, null, crosspost);
 	}
 
@@ -259,17 +258,17 @@ public class PostSent {
 		this(body, title, null, null, rtl, created, null);
 	}
 
-	public PostSent(final String   body,
-	                final String   title,
-	                final Boolean  rtl,
-	                final Pair<CrosspostID, String> crosspost) {
+	public PostSent(final String          body,
+	                final String          title,
+	                final Boolean         rtl,
+	                final CrosspostInfo[] crosspost) {
 		this(body, title, null, null, rtl, null, crosspost);
 	}
 
-	public PostSent(final String        body,
-	                final String        title,
-	                final ZonedDateTime created,
-	                final Pair<CrosspostID, String>      crosspost) {
+	public PostSent(final String          body,
+	                final String          title,
+	                final ZonedDateTime   created,
+	                final CrosspostInfo[] crosspost) {
 		this(body, title, null, null, null, created, crosspost);
 	}
 
@@ -287,10 +286,10 @@ public class PostSent {
 		this(body, null, font, lang, null, created, null);
 	}
 
-	public PostSent(final String     body,
-	                final Appearance font,
-	                final Locale     lang,
-	                final Pair<CrosspostID, String>   crosspost) {
+	public PostSent(final String          body,
+	                final Appearance      font,
+	                final Locale          lang,
+	                final CrosspostInfo[] crosspost) {
 		this(body, null, font, lang, null, null, crosspost);
 	}
 
@@ -301,17 +300,17 @@ public class PostSent {
 		this(body, null, font, null, rtl, created, null);
 	}
 
-	public PostSent(final String     body,
-	                final Appearance font,
-	                final Boolean    rtl,
-	                final Pair<CrosspostID, String>   crosspost) {
+	public PostSent(final String          body,
+	                final Appearance      font,
+	                final Boolean         rtl,
+	                final CrosspostInfo[] crosspost) {
 		this(body, null, font, null, rtl, null, crosspost);
 	}
 
-	public PostSent(final String        body,
-	                final Appearance    font,
-	                final ZonedDateTime created,
-	                final Pair<CrosspostID, String>      crosspost) {
+	public PostSent(final String          body,
+	                final Appearance      font,
+	                final ZonedDateTime   created,
+	                final CrosspostInfo[] crosspost) {
 		this(body, null, font, null, null, created, crosspost);
 	}
 
@@ -322,24 +321,24 @@ public class PostSent {
 		this(body, null, null, lang, rtl, created, null);
 	}
 
-	public PostSent(final String   body,
-	                final Locale   lang,
-	                final Boolean  rtl,
-	                final Pair<CrosspostID, String> crosspost) {
+	public PostSent(final String          body,
+	                final Locale          lang,
+	                final Boolean         rtl,
+	                final CrosspostInfo[] crosspost) {
 		this(body, null, null, lang, rtl, null, crosspost);
 	}
 
-	public PostSent(final String        body,
-	                final Locale        lang,
-	                final ZonedDateTime created,
-	                final Pair<CrosspostID, String>      crosspost) {
+	public PostSent(final String          body,
+	                final Locale          lang,
+	                final ZonedDateTime   created,
+	                final CrosspostInfo[] crosspost) {
 		this(body, null, null, lang, null, created, crosspost);
 	}
 
-	public PostSent(final String        body,
-	                final Boolean       rtl,
-	                final ZonedDateTime created,
-	                final Pair<CrosspostID, String>      crosspost) {
+	public PostSent(final String          body,
+	                final Boolean         rtl,
+	                final ZonedDateTime   created,
+	                final CrosspostInfo[] crosspost) {
 		this(body, null, null, null, rtl, created, crosspost);
 	}
 
@@ -367,9 +366,9 @@ public class PostSent {
 		this(body, title, null, null, null, created, null);
 	}
 
-	public PostSent(final String   body,
-	                final String   title,
-	                final Pair<CrosspostID, String> crosspost) {
+	public PostSent(final String          body,
+	                final String          title,
+	                final CrosspostInfo[] crosspost) {
 		this(body, title, null, null, null, null, crosspost);
 	}
 
@@ -391,9 +390,9 @@ public class PostSent {
 		this(body, null, font, null, null, created, null);
 	}
 
-	public PostSent(final String     body,
-	                final Appearance font,
-	                final Pair<CrosspostID, String>   crosspost) {
+	public PostSent(final String          body,
+	                final Appearance      font,
+	                final CrosspostInfo[] crosspost) {
 		this(body, null, font, null, null, null, crosspost);
 	}
 
@@ -409,9 +408,9 @@ public class PostSent {
 		this(body, null, null, lang, null, created, null);
 	}
 
-	public PostSent(final String   body,
-	                final Locale   lang,
-	                final Pair<CrosspostID, String> crosspost) {
+	public PostSent(final String          body,
+	                final Locale          lang,
+	                final CrosspostInfo[] crosspost) {
 		this(body, null, null, lang, null, null, crosspost);
 	}
 
@@ -421,15 +420,15 @@ public class PostSent {
 		this(body, null, null, null, rtl, created, null);
 	}
 
-	public PostSent(final String   body,
-	                final Boolean  rtl,
-	                final Pair<CrosspostID, String> crosspost) {
+	public PostSent(final String          body,
+	                final Boolean         rtl,
+	                final CrosspostInfo[] crosspost) {
 		this(body, null, null, null, rtl, null, crosspost);
 	}
 
-	public PostSent(final String        body,
-	                final ZonedDateTime created,
-	                final Pair<CrosspostID, String>      crosspost) {
+	public PostSent(final String          body,
+	                final ZonedDateTime   created,
+	                final CrosspostInfo[] crosspost) {
 		this(body, null, null, null, null, created, crosspost);
 	}
 
@@ -458,8 +457,8 @@ public class PostSent {
 		this(body, null, null, null, null, created, null);
 	}
 
-	public PostSent(final String   body,
-	                final Pair<CrosspostID, String> crosspost) {
+	public PostSent(final String          body,
+	                final CrosspostInfo[] crosspost) {
 		this(body, null, null, null, null, null, crosspost);
 	}
 
@@ -467,31 +466,31 @@ public class PostSent {
 		this(body, null, null, null, null, null, null);
 	}
 
-	public final String        getBody() {
+	public final String          getBody() {
 		return this.body;
 	}
 
-	public final String        getTitle() {
+	public final String          getTitle() {
 		return this.title;
 	}
 
-	public final Appearance    getFont() {
+	public final Appearance      getFont() {
 		return this.font;
 	}
 
-	public final Locale        getLang() {
+	public final Locale          getLang() {
 		return this.lang;
 	}
 
-	public final Boolean       getRtl() {
+	public final Boolean         getRtl() {
 		return this.rtl;
 	}
 
-	public final ZonedDateTime getCreated() {
+	public final ZonedDateTime   getCreated() {
 		return this.created;
 	}
 
-	public final Pair<CrosspostID, String>      getCrosspostInfo() {
+	public final CrosspostInfo[] getCrosspostInfo() {
 		return this.crosspost;
 	}
 }
