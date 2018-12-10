e25c4d86cd1e4635486bb5371b2399182995ce93
Wamm K. D <jaft.r@outlook.com>
2018-12-09 23:42:51 -0600

    I'm not sure how `Gson` figures out how to map what to what so I'm just gonna rename this one in case it matters.

--- src/main/java/fediverse/writefreely/api/model/User.java
+++ src/main/java/fediverse/writefreely/api/model/User.java
@@ -3,21 +3,21 @@ package fediverse.writefreely.api.model;
 import java.time.ZonedDateTime;
 
 public class User {
-	private final String        name;
+	private final String        username;
 	private final Boolean       hasPass;
 	private final String        eMail;
 	private final ZonedDateTime created;
 
-	public User(final String name,  final Boolean       hasPass,
-	            final String eMail, final ZonedDateTime created) {
-		this.name    = name;
-		this.hasPass = hasPass;
-		this.eMail   = eMail;
-		this.created = created;
+	public User(final String username, final Boolean       hasPass,
+	            final String email,    final ZonedDateTime created) {
+		this.username = username;
+		this.hasPass  = hasPass;
+		this.eMail    = email;
+		this.created  = created;
 	}
 
-	public final String        getName() {
-		return this.name;
+	public final String        getUsername() {
+		return this.username;
 	}
 
 	public final Boolean       doesHavePass() {
